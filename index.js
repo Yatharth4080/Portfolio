07c9c1b07ead476bf54baf9f95ca5d09642ca2b6
Yatharth4080 <iampuneet982@gmail.com>
2023-04-28 16:03:35 +0530

    final changes

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 5500,
+      delay: 3500,
       disableOnInteraction: false,
     },
     pagination: {
